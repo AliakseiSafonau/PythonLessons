159b7b523081491f829a693aa78a6531b36914bb
AliakseiSafonau <pharmaceftssma@gmail.com>
2023-03-08 23:01:53 +0300

    add palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,11 @@
-print('Hello world!')
+def palindrome_search(text):
+    text = text.replace(' ', '')
+    result = 'True'
+    for i in range(0, len(text) // 2):
+        if text[i] != text[-1 - i]:
+            result = False
+            break
+    print(result)
+
+
+palindrome_search('fghj hgf]')
